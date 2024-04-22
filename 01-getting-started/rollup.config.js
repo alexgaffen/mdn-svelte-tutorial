d2cfd010bcabb6884d3aeafbdbe0973cba12b1c7
Alex Gaffen <84543637+alexgaffen@users.noreply.github.com>
2024-04-22 12:46:00 -0400

    hi

--- 01-getting-started/rollup.config.js
+++ 01-getting-started/rollup.config.js
@@ -22,7 +22,14 @@ export default {
 			// a separate file - better for performance
 			css: css => {
 				css.write('public/build/bundle.css');
-			}
+			},
+
+			onwarn: (warning, handler) => {
+				if (warning.code === "a11y-missing-attribute") {
+					return;
+				}
+				handler(warning);				
+			},
 		}),
 
 		// If you have external dependencies installed from
